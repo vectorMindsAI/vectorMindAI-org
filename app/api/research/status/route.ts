6d6b8bf2465e9f2807ae5c27a50a201badd7ae57
Priyansh87 <priyansh56701@gmail.com>
2026-01-04 00:31:23 +0530

    bug fixed: incorrect response

--- app/api/research/status/route.ts
+++ app/api/research/status/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { jobStore } from "@/lib/store";
 
+export const dynamic = "force-dynamic";
+
 export const GET = async (req: Request) => {
     const { searchParams } = new URL(req.url);
     const id = searchParams.get("id");
